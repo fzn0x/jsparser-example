c249cc16b36a1d1a906877d5a5c34db9faa5609d
Muhammad Fauzan <developerfauzan@gmail.com>
2021-05-02 15:49:18 +0700

    add result and example comments in bdd.js

--- bdd.js
+++ bdd.js
@@ -2,5 +2,8 @@ const { Parser } = require("./Parser");
 const parser = new Parser();
 
 let program = parser.parse(`31`);
+// let program = parser.parse(`"String"`);
+// let program = parser.parse(`'String'`);
 
 console.log(JSON.stringify(program, 0, 2));
+// result : NumericLiteral AST
